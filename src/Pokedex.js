981e31b0b3fb849c63cbcd2cacb0e4055171ea60
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-07 20:47:53 -0400

    Soluçao dos exercicios 1, 2 e bonus do dia 11.1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,79 +1,73 @@
-import PropTypes from 'prop-types';
-import Pokemon from './Pokemon';
-import React from 'react';
-import data from './data';
-import './PokedexCSS.css'
+import React from "react";
+import Pokemon from "./Pokemon";
+import Button from "./Button";
+import pokemons from "./data";
 
 class Pokedex extends React.Component {
-  constructor() {
-    super()
-
-    this.types = ['All', ...new Set(data.map(pokemon => pokemon.type))].sort();
+  constructor(props) {
+    super(props);
     this.state = {
-      pokemonsTypes: data,
-      position: 0,
+      pokemonIndex: 0,
+      pokemonType: "all",
     };
-
-    this.nextPokemon = this.nextPokemon.bind(this);
-    this.pokemonsByType = this.pokemonsByType.bind(this);
+    this.filteredPokemon = this.fetchFilteredPokemon.bind(this);
+    this.setAllPokemons = this.setAllPokemons.bind(this);
+    this.carousel = this.carousel.bind(this);
+    this.setPokemonState = this.setPokemonState.bind(this);
   }
 
-  nextPokemon() {
-    const { pokemonsTypes } = this.state;
-    this.setState((previousState, _props) => {
-      const { position } = previousState;
-      const indexAtual = position === pokemonsTypes.length - 1 ? 0 : position + 1;
-      return { ...previousState, position: indexAtual };
-    }
-    )
+  fetchFilteredPokemon() {
+    const { pokemons } = this.props;
+    const { pokemonType } = this.state;
+
+    return pokemons.filter((pokemon) => {
+      if (pokemonType === "all") return true;
+      return pokemon.type === pokemonType;
+    });
   }
 
-  pokemonsByType(event) {
-    const currentType = event.target.id;
-    const filtredsPokemons = data.filter((pokemon) => pokemon.type === currentType);
-    currentType !== 'All'
-    ? 
-    this.setState(() => ({
-      position: 0,
-      pokemonsTypes: filtredsPokemons,
-    }))
-    :
-    this.setState(() => ({
-      position: 0,
-      pokemonsTypes: data,
-  }))
-};
+  setAllPokemons() {
+    this.setState({ pokemonType: "all" });
+  }
+
+  carousel(btnClicked) {
+    const { pokemonIndex } = this.state;
+    const filteredPokemon = this.fetchFilteredPokemon();
+    const pokemon = filteredPokemon;
+    if (pokemonIndex + btnClicked === pokemon.length)
+      return this.setState({ pokemonIndex: 0 });
+    if (pokemonIndex + btnClicked < 0)
+      return this.setState({ pokemonIndex: pokemon.length - 1 });
+    this.setState({ pokemonIndex: pokemonIndex + btnClicked });
+  }
+
+  setPokemonState(type) {
+    this.setState({ pokemonType: type, pokemonIndex: 0 });
+  }
 
   render() {
+    const filteredPokemon = this.fetchFilteredPokemon();
+    const pokemon = filteredPokemon[this.state.pokemonIndex];
+    const newArrayPokemons = [...new Set(pokemons.reduce((types, { type }) => [...types, type], []))];
+
     return (
-      <>
-        <div className="h1Div">
-        <h1>POKEDEX</h1>
+      <div className="pokedex">
+        <div className="pokedex-buttons-panel">
+          <Button onClick={() => this.carousel(-1)} className="btn-previous-next">Previous Pokemon</Button>
+          <Button onClick={() => this.carousel(1)} className="btn-previous-next">Next Pokemon</Button>
         </div>
-        <div className="App">
-          <Pokemon poke={this.state.pokemonsTypes[this.state.position]} />
+        <div className="pokemon-card">
+          <Pokemon pokemon={pokemon} />
         </div>
-        <div className="buttonsTypes row-btn-types">
-          {this.types.map((tipo) => <button key={tipo} id={tipo} onClick={this.pokemonsByType}>{tipo}</button>)}
-        </div>
-        <div className="row-btn-next">
-          <button className="btn-next" onClick={this.nextPokemon}>Next Pokemon!</button>
-        </div>
-      </>
-    )
+        <Button onClick={this.setAllPokemons} className="btn-types">All</Button>
+        {newArrayPokemons.map((type) => (
+          <Button onClick={() => this.setPokemonState(type)} key={type} className="btn-types">
+            {type}
+          </Button>
+        ))}
+      </div>
+    );
   }
 }
-
-Pokedex.propTypes = {
-  pokemon: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    averageWeight: PropTypes.shape({
-      value: PropTypes.number.isRequired,
-      measurementUnit: PropTypes.string.isRequired,
-    }).isRequired,
-    image: PropTypes.string.isRequired,
-  })).isRequired,
-};
 
 export default Pokedex;
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,31 +1,23 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
 
-class Pokemon extends Component {
+class Pokemon extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    const { name, type, averageWeight, image} = this.props.poke;
-    const { value, measurementUnit } = averageWeight;
+    const { name, type, averageWeight, image } = this.props.pokemon;
     return (
-      <div className='pokemon'>
-        <h1>{name}</h1>
-        <h2>{type}</h2>
-        <p>{value}{measurementUnit}</p>
-        <img className = "pokemon-img" src={image} alt='imagem do pokemon' />
+      <div className="pokemon">
+        <div>
+          <p> {name} </p>
+          <p> {type} </p>
+          <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`} </p>
+        </div>
+        <img src={image} alt={`${name} sprite`} />
       </div>
     );
   }
 }
-
-Pokemon.propTypes = {
-  poke: PropTypes.shape({
-  name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  averageWeight: PropTypes.shape({
-    value: PropTypes.number.isRequired,
-    measurementUnit: PropTypes.string.isRequired,
-  }).isRequired,
-  image: PropTypes.string.isRequired,
-  }).isRequired,
-};
 
 export default Pokemon;
